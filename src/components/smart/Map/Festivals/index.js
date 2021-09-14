bc578774197b5054b50638d8761eb059fc98fdef
Seedy <seedy.dupuis@gmail.com>
2021-09-14 22:26:44 +0200

    festivals: places and pictures

--- src/components/smart/Map/Festivals/index.js
+++ src/components/smart/Map/Festivals/index.js
@@ -1,14 +1,20 @@
+import clsx from 'clsx';
+
 import makeStyles from '@material-ui/core/styles/makeStyles';
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+import BuildIcon from '@material-ui/icons/Build';
 
 // CONSTANTS
 const PLACES = [
-  { coordinates: [47.510, -1.419], title: 'Dub Camp', subtitle: 'Le plus grand festival de dub et sound system en France', volunteer: true },
-  { coordinates: [45.559, 5.286], title: 'Bruit du Marteau', subtitle: 'Un festival de montage d\'échafaudages', volunteer: true },
+  { coordinates: [47.510, -1.419], title: 'Dub Camp', subtitle: 'Le plus grand festival de dub et sound system en France', volunteer: true, img: 'festivals/dubcamp.jpg' },
+  { coordinates: [45.559, 5.286], title: 'Bruit du Marteau', subtitle: 'Un festival de montage d\'échafaudages', volunteer: true, img: 'festivals/bdm.jpg' },
   { coordinates: [48.833, 2.445], title: 'We Love Green', subtitle: 'Musiques électroniques et écologie' },
   { coordinates: [48.582, -3.834], title: 'Panorama', subtitle: 'Techno, toujours pareil' },
-  { coordinates: [45.783, 4.876], title: '24 Heures de L\'INSA', subtitle: 'Mon tout premier', volunteer: true },
+  { coordinates: [45.784, 4.876], title: '24 Heures de L\'INSA', subtitle: 'Mon tout premier', volunteer: true, img: 'festivals/24.jpg' },
   { coordinates: [45.783, 4.876], title: 'Karnaval Humanitaire', subtitle: 'Le festival des copains, écolo, humanitaire', volunteer: true },
   { coordinates: [45.779, 4.924], title: 'Archisound', subtitle: 'Organisé par les copains architectes' },
   { coordinates: [45.611, 4.706], title: 'FestBouc', subtitle: 'Une grosse expérience en cuisine', volunteer: true },
@@ -19,9 +25,9 @@ const PLACES = [
   { coordinates: [48.827, 2.216], title: 'Rock en Seine', subtitle: 'Rock et musiques actuelles' },
   { coordinates: [48.856, 2.232], title: 'Solidays', subtitle: 'Musiques actuelles et solidarité sida' },
   { coordinates: [46.522, 5.617], title: 'Mousse toi là', subtitle: 'Mini festival organisé par la Brasserie des Trois Epis' },
-  { coordinates: [49.179, -0.224], title: 'Rastart', subtitle: 'Festival reggae et sound system engagé', volunteer: true },
-  { coordinates: [44.891, 13.797], title: 'Outlook', subtitle: 'La plus grande célébration européenne de la culture Sound System dans un fort' },
-  { coordinates: [48.902, 2.367], title: 'Forward - bass culture', subtitle: 'Festival Sound System à Paris, anciennement Telerama Dub' },
+  { coordinates: [49.179, -0.224], title: 'Rastart', subtitle: 'Festival reggae et sound system engagé', volunteer: true, img: 'festivals/rastart.jpg', reverse: true },
+  { coordinates: [44.891, 13.797], title: 'Outlook', subtitle: 'La plus grande célébration européenne de la culture Sound System dans un fort', img: 'festivals/outlook.jpg' },
+  { coordinates: [48.902, 2.367], title: 'Forward - bass culture', subtitle: 'Festival Sound System à Paris, anciennement Telerama Dub', img: 'festivals/forward.jpg' },
   { coordinates: [47.076, 2.397], title: 'Printemps de Bourges', subtitle: 'Musiques électroniques' },
   { coordinates: [47.230, 6.038], title: 'Détonation', subtitle: 'Dub et électro' },
 
@@ -29,32 +35,55 @@ const PLACES = [
 
 // HOOKS
 const useStyles = makeStyles(() => ({
-  mapContainer: {
-    height: 500,
+  imgSmall: {
+    width: 200,
+    height: 150,
+  },
+  imgReverse: {
+    width: 150,
+    height: 200,
+  },
+  textSecondaryPopup: {
+    color: 'rgba(0, 0, 0, 0.54)',
   },
 }));
 
 // COMPONENTS
-const MapFestivals = () => {
+const MapFestivals = (props) => {
   const classes = useStyles();
 
   return (
     <MapContainer
-      className={classes.mapContainer}
       center={[45.9, 4.59]}
       zoom={4}
       scrollWheelZoom
+      {...props}
     >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {PLACES.map(({ coordinates, title, subtitle }) => (
+      {PLACES.map(({ coordinates, title, subtitle, img, reverse, volunteer }) => (
         <Marker key={title} position={coordinates}>
           <Popup>
-            {title}
-            <br />
-            {subtitle}
+            <Box width="100%" height="100%" display="flex" flexDirection="column" alignItems="center">
+              <Typography variant="h6">
+                {title}
+                {volunteer && (
+                  <Box display="inline" ml={1}>
+                    <BuildIcon color="primary" />
+                  </Box>
+                )}
+              </Typography>
+              <Typography variannt="subtitle1" className={classes.textSecondaryPopup}>{subtitle}</Typography>
+              {img && (
+                <img
+                  className={clsx(classes.imgSmall, { [classes.imgReverse]: reverse })}
+                  src={img}
+                  alt={title}
+                />
+              )}
+            </Box>
           </Popup>
         </Marker>
       ))}
