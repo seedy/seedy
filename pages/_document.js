3cb44cebdb9154cc5295dbd1e2c481a3781dcc6e
Seedy <seedy.dupuis@gmail.com>
2021-09-26 14:48:24 +0200

    ssr: integrate mui and emotion with next ssr

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,45 @@
+import createEmotionCache from 'helpers/createEmotionCache';
+import createEmotionServer from '@emotion/server/create-instance';
+
+import { Children } from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+// DOCUMENT
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    // integrate @mui and @emotion config with SSR
+    const originalRenderPage = ctx.renderPage;
+
+    // You can consider sharing the same emotion cache between all the SSR requests
+    // to speed up performance.
+    // However, be aware that it can have global side effects.
+    const cache = createEmotionCache();
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
+    ctx.renderPage = () => originalRenderPage({
+      enhanceApp: (App) => (props) => <App emotionCache={cache} {...props} />,
+    });
+
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+
+    // This is important. It prevents emotion to render invalid HTML.
+    // See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+      <style
+        data-emotion={`${style.key} ${style.ids.join(' ')}`}
+        key={style.key}
+      // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: style.css }}
+      />
+    ));
+
+
+    return {
+      ...initialProps,
+      // Styles fragment is rendered after the app and page rendering finish.
+      styles: [...Children.toArray(initialProps.styles), ...emotionStyleTags],
+    };
   }
 
   render() {
