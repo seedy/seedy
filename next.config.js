c564e5539a45323a7b8575e2655adcae4e0f464f
Seedy <seedy.dupuis@gmail.com>
2021-09-26 20:26:06 +0200

    i18n: config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const withTM = require('next-transpile-modules')(['@mui/material']);
+const { i18n } = require('./next-i18next.config');
 
 module.exports = withTM({
   reactStrictMode: true,
+  i18n,
   images: {
     domains: ['www.codewars.com'],
   },
