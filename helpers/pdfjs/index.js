325da55242812acc71b2a8134c0d7a672027a874
Seedy <seedy.dupuis@gmail.com>
2022-04-02 19:21:43 +0200

    chore: lint unused code

--- helpers/pdfjs/index.js
+++ helpers/pdfjs/index.js
@@ -8,26 +8,3 @@ if (typeof window !== 'undefined' && 'Worker' in window) {
 
 
 export const getDocument = (pdfFile) => pdfjsLib.getDocument(pdfFile).promise;
-
-export const getPage = async (page, pdfDoc, canvas) => {
-  const pdfPage = await pdfDoc.getPage(page);
-  const viewport = pdfPage.getViewport({ scale: 1.0 });
-  canvas.width = viewport.width; // eslint-disable-line no-param-reassign
-  canvas.height = viewport.height; // eslint-disable-line no-param-reassign
-  const canvasContext = canvas.getContext('2d');
-
-  const { promise } = pdfPage.render({
-    canvasContext,
-    viewport,
-  });
-  return promise;
-};
-
-export const getDocFirstPage = async (pdfFile, canvas) => {
-  const pdfDoc = await getDocument(pdfFile);
-  const task = getPage(1, pdfDoc, canvas);
-  return {
-    pdfDoc,
-    task,
-  };
-};
