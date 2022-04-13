c6a63addfa67378d856e430e3d708574045ed453
Seedy <seedy.dupuis@gmail.com>
2022-04-14 00:17:15 +0200

    feat: images, optimization

--- components/dumb/ImageList/Interests/index.js
+++ components/dumb/ImageList/Interests/index.js
@@ -11,15 +11,15 @@ import CopyrightIcon from '@mui/icons-material/Copyright';
 
 // CONSTANTS
 const ITEMS = [
-  { title: 'uxDesign', img: '/tiles/1.jpg', info: 'thabor', date: 2020, copyright: 'Cédric DUPUIS', width: 350, height: 244 },
-  { title: 'graphicalTrends', img: '/tiles/2.jpg', info: 'ticketToRide', date: 2021, copyright: 'Cédric DUPUIS', width: 400, height: 300 },
-  { title: 'a11yPatterns', img: '/tiles/3.jpg', info: 'viaduct', date: 2021, copyright: 'Cédric DUPUIS', width: 400, height: 300 },
-  { title: 'cleanCode', img: '/tiles/4.jpg', info: 'tomatoSauce', date: 2020, copyright: 'Cédric DUPUIS', width: 300, height: 350 },
-  { title: 'componentLib', img: '/tiles/5.jpg', info: 'tomatoHouse', date: 2021, copyright: 'Cédric DUPUIS', width: 300, height: 200 },
-  { title: 'techChallenges', img: '/tiles/6.jpg', info: 'forest', date: 2021, copyright: 'Cédric DUPUIS', width: 400, height: 300 },
-  { title: 'enjoyWork', img: '/tiles/7.jpg', info: 'pond', date: 2021, copyright: 'Cédric DUPUIS', width: 400, height: 300 },
-  { title: 'timeProductivity', img: '/tiles/8.jpg', info: 'kale', date: 2021, copyright: 'Cédric DUPUIS', width: 400, height: 300 },
-  { title: 'workIdeals', img: '/tiles/9.jpg', info: 'cape', date: 2021, copyright: 'Cédric DUPUIS', width: 400, height: 300 },
+  { title: 'uxDesign', img: '/tiles/1.jpg', info: 'thabor', date: 2020, copyright: 'Cédric DUPUIS', width: 300, height: 225 },
+  { title: 'graphicalTrends', img: '/tiles/2.jpg', info: 'ticketToRide', date: 2021, copyright: 'Cédric DUPUIS', width: 300, height: 225 },
+  { title: 'a11yPatterns', img: '/tiles/3.jpg', info: 'viaduct', date: 2021, copyright: 'Cédric DUPUIS', width: 300, height: 225 },
+  { title: 'cleanCode', img: '/tiles/4.jpg', info: 'tomatoSauce', date: 2020, copyright: 'Cédric DUPUIS', width: 300, height: 225 },
+  { title: 'componentLib', img: '/tiles/5.jpg', info: 'tomatoHouse', date: 2021, copyright: 'Cédric DUPUIS', width: 300, height: 225 },
+  { title: 'techChallenges', img: '/tiles/6.jpg', info: 'forest', date: 2021, copyright: 'Cédric DUPUIS', width: 300, height: 225 },
+  { title: 'enjoyWork', img: '/tiles/7.jpg', info: 'pond', date: 2021, copyright: 'Cédric DUPUIS', width: 300, height: 225 },
+  { title: 'timeProductivity', img: '/tiles/8.jpg', info: 'kale', date: 2021, copyright: 'Cédric DUPUIS', width: 300, height: 225 },
+  { title: 'workIdeals', img: '/tiles/9.jpg', info: 'cape', date: 2021, copyright: 'Cédric DUPUIS', width: 300, height: 225 },
 ];
 
 const BACK_PROPS = {
@@ -31,11 +31,11 @@ const BACK_PROPS = {
 
 
 // COMPONENTS
-const ImageListInterests = ({ rowHeight, ...props }) => {
+const ImageListInterests = (props) => {
   const { t } = useTranslation('interests');
 
   return (
-    <ImageList sx={{ m: 0 }} rowHeight={rowHeight} {...props}>
+    <ImageList sx={{ m: 0 }} {...props}>
       {ITEMS.map(({ title, img, info, date, copyright, ...rest }) => (
         <ImageListItemFlippable
           key={img}
@@ -65,12 +65,10 @@ const ImageListInterests = ({ rowHeight, ...props }) => {
 };
 
 ImageListInterests.propTypes = {
-  rowHeight: PropTypes.number,
   cols: PropTypes.number,
 };
 
 ImageListInterests.defaultProps = {
-  rowHeight: 180,
   cols: 1,
 };
 
